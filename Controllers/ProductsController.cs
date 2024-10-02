c56d9084e6c7a62afba182e08cb1f9ae541f171b
Choopa <djordje.jokic@gmail.com>
2024-10-02 15:33:34 +0200

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReStoreAPI.Data;
+using ReStoreAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
